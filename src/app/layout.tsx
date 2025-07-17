53789314e74c00ce440f23445cb121c2b2dea547
lance.guinto <aldrin@secret6.com>
2025-07-18 04:06:42 +0800

    Added sample question js for testing. Added react toast component. Updated package.json.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Toaster } from 'react-hot-toast';
 
 
 export const metadata: Metadata = {
-  title: "PDF Quiz Generator",
-  description: "Generate quizzes from PDFs",
+	title: "PDF Quiz Generator",
+	description: "Generate quizzes from PDFs",
 };
 
 export default function RootLayout({
@@ -14,11 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+		<body className={`antialiased`}>
+			<Toaster position="bottom-center" />
+			{children}
+		</body>
+	</html>
   );
 }
--- src/components/submissionForm.tsx
+++ src/components/submissionForm.tsx
@@ -1,27 +1,73 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { promises as fs } from "fs";
 
-export default function SubmissionForm() {
-	async function action(formData: FormData) {
-		"use server";
-		const file = formData.get("file") as File;
-		console.log(file);
+// export default async function SubmissionForm() {
+// 	async function action(formData: FormData) {
+// 		"use server";
+// 		const file = formData.get("file") as File;
+// 		console.log(file);
 
-		if (!file || file.size === 0) {
-			return { error: "No file uploaded." }
-		}
+// 		if (!file || file.size === 0) {
+// 			return { error: "No file uploaded." }
+// 		}
 
-		const data = await file.arrayBuffer();
-		await fs.writeFile(`${process.cwd}/tmp/${file.name}`, Buffer.from(data));
-	} 
+// 		const data = await file.arrayBuffer();
+// 		await fs.writeFile(`${process.cwd}/tmp/${file.name}`, Buffer.from(data));
+// 	} 
 
-	return (
+// 	return (
+// 		<div>
+// 		<form action={action}>
+// 			<Input type="file" name="file" accept=".pdf"/>
+// 			<Button>Upload</Button>
+// 		</form>
+// 		</div>
+// 	);
+// }
+
+
+import React from "react";
+import toast from "react-hot-toast";
+
+function SubmissionForm() {
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (file) {
+            toast.success(`File ${file.name} uploaded successfully`);
+        }
+    }
+	
+  	return (
 		<div>
-		<form action={action}>
-			<Input type="file" name="file" accept=".pdf"/>
-			<Button>Upload</Button>
-		</form>
+			<Input type="file" name="file" accept=".pdf" onChange={handleFileChange}/>
+			{/* <input
+				type="radio"
+				name="quiz_generator_tabs"
+				className="tab"
+				aria-label="Educator"
+				defaultChecked
+			/>
+			<div className="tab-content bg-base-100 border-base-300 p-6">
+				<fieldset className="fieldset">
+				<legend className="fieldset-legend">Pick a file</legend>
+				<input type="file" className="file-input" onChange={handleFileChange} />
+				<label className="label">Max size 32MB</label>
+				</fieldset>
+			</div>
+
+			<input
+				type="radio"
+				name="quiz_generator_tabs"
+				className="tab"
+				aria-label="Student"
+			/>
+			<div className="tab-content bg-base-100 border-base-300 p-6">
+				Tab content 2
+			</div> */}
 		</div>
-	);
+  	);
 }
+
+export default SubmissionForm;
